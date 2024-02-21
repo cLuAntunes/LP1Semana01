c64c17a6f0fc3ba0e9d3ebd5e7ea4d0e2562891f
Clara Antunes <claraantunes.work@gmail.com>
2024-02-21 17:59:28 +0000

    Add comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
