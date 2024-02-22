55d3f78cfb5dbbcc3d44cd65b774263c52899a1c
Clara Antunes <claraantunes.work@gmail.com>
2024-02-22 11:10:11 +0000

    Add Integral Types

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numerobyte = 10;
+            sbyte numerosbyte = -10;
+            int numeroint = 20;
+            uint numerounint = 30u;
+            long numerolong = 40L;
+            ulong numeroulong = 50UL;
+            short numeroshort = 60;
+            ushort numeroushort = 70;
+
+            Console.WriteLine(numerobyte);
+            Console.WriteLine(numerosbyte);
+            Console.WriteLine(numeroint);
+            Console.WriteLine(numerounint);
+            Console.WriteLine(numerolong);
+            Console.WriteLine(numeroulong);
+            Console.WriteLine(numeroshort);
+            Console.WriteLine(numeroushort);
+
+        }
+    }
+}
