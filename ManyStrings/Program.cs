ca68b34ae746939ccdf1c7eb11bdfbe8526e0caa
Clara Antunes <claraantunes.work@gmail.com>
2024-02-22 20:23:52 +0000

    Add String Tests

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sign = "The sign says \"Hello World!\"";
+            string instruction = "Follow the arrows!\u27A5\u27A5\u27A5";
+            string warning = "\u274CDon't touch the grass!\u274C";
+            string message = "Kindly check the attached \"Love Letter\"\u2765";
+            string path = "Check: C:\\ProgramData\\Microsoft\\Windows";
+
+            Console.WriteLine(sign);
+            Console.WriteLine(instruction);
+            Console.WriteLine(warning);
+            Console.WriteLine(message);
+            Console.WriteLine(path);
+
+        }
+    }
+}
