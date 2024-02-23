0b74e087408da4c4d6bf8cf8338f8ba3a0b212d9
Clara Antunes <claraantunes.work@gmail.com>
2024-02-23 16:42:38 +0000

    Add Ask for Real Number

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -11,7 +11,12 @@ namespace AskForInput
 
             int inteiro = int.Parse(inp);
 
-            
+            Console.WriteLine("Por favor, insira um número real: ");
+            string rl = Console.ReadLine();
+
+            float real = float.Parse(rl);
+
+
         }
     }
 }
