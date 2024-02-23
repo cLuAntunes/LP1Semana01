9ef4e7c0f621bb6f302d937ad05eea23e459f2de
Clara Antunes <claraantunes.work@gmail.com>
2024-02-23 17:03:50 +0000

    Add String Input and Int Conversion

--- DecimalHexa/Program.cs
+++ DecimalHexa/Program.cs
@@ -6,7 +6,20 @@ namespace DecimalHexa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insira quatro números inteiros: ");
+            
+            string numero1 = Console.ReadLine();
+            string numero2 = Console.ReadLine();
+            string numero3 = Console.ReadLine();
+            string numero4 = Console.ReadLine();
+
+            
+            int num1 = int.Parse(numero1);
+            int num2 = int.Parse(numero2);
+            int num3 = int.Parse(numero3);
+            int num4 = int.Parse(numero4);
+
+
         }
     }
 }
